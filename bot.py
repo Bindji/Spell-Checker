6f0495df64b9ff3504ce89cbd40e2a5b27fe828b
JK DEVELOPER <123283810+Bindji@users.noreply.github.com>
2025-12-15 07:27:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,13 +9,6 @@ API_ID = 21692958
 API_HASH = "7aafe6264759352e9459eb434457c8f3"
 BOT_TOKEN = "7140468132:AAEBJRdQLmBbeYh1Us-LpU2NlKaG1fK-8AY"
 
-# ---------- Web Server (Koyeb / Render) ----------
-"""async def handle(request):
-    return web.Response(text="I'm alive!")
-
-app = web.Application()
-app.router.add_get("/", handle)"""
-
 # ---------------- BOT -------------------
 bot = Client(
     "MovieCorrectorBot",
@@ -24,19 +17,4 @@ bot = Client(
     bot_token=BOT_TOKEN,
     plugins={"root": "plugins"}
 )
-
-# -------- Run Everything --------
-"""async def main():
-    await bot.start()
-    print("🤖 Bot Started")
-
-    runner = web.AppRunner(app)
-    await runner.setup()
-    site = web.TCPSite(runner, port=8080)
-    await site.start()
-    print("🌐 Web Server Started")
-
-    await idle()   # ✅ correct usage
-
-asyncio.run(main())"""
 bot.run()
