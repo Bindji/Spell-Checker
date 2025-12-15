f611a0830e0223d4592682dbfb84777a179263ea
JK DEVELOPER <123283810+Bindji@users.noreply.github.com>
2025-12-15 07:30:27 +0530

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -2,9 +2,15 @@ from pyrogram import Client, filters
 import asyncio
 from imdb import IMDb
 from aiohttp import web
+import re
+from imdb import IMDb
+import asyncio
+import json
 
 imdb = IMDb()
 CACHE = {}
+MAX_LIST_ELM = None
+LONG_IMDB_DESCRIPTION = False
 
 def correct_movie_name(query: str):
     query = query.strip()
@@ -25,25 +31,115 @@ def correct_movie_name(query: str):
     except Exception as e:
         print("IMDb Error:", e)
         return None
-      
+
+def list_to_str(k):
+    if not k:
+        return "N/A"
+    elif len(k) == 1:
+        return str(k[0])
+    else:
+        return ' '.join(f'{elem}, ' for elem in k)
+        
+async def get_poster(query, bulk=False, id=False, file=None):
+    if not id:
+        query = (query.strip()).lower()
+        title = query
+        year = re.findall(r'[1-2]\d{3}$', query, re.IGNORECASE)
+        if year:
+            year = list_to_str(year[:1])
+            title = (query.replace(year, "")).strip()
+        elif file is not None:
+            year = re.findall(r'[1-2]\d{3}', file, re.IGNORECASE)
+            if year:
+                year = list_to_str(year[:1]) 
+        else:
+            year = None
+        movieid = imdb.search_movie(title.lower(), results=10)
+        if not movieid:
+            return None
+        if year:
+            filtered=list(filter(lambda k: str(k.get('year')) == str(year), movieid))
+            if not filtered:
+                filtered = movieid
+        else:
+            filtered = movieid
+        movieid=list(filter(lambda k: k.get('kind') in ['movie', 'tv series'], filtered))
+        if not movieid:
+            movieid = filtered
+        if bulk:
+            return movieid
+        movieid = movieid[0].movieID
+    else:
+        movieid = query
+    movie = imdb.get_movie(movieid)
+    if movie.get("original air date"):
+        date = movie["original air date"]
+    elif movie.get("year"):
+        date = movie.get("year")
+    else:
+        date = "N/A"
+    plot = ""
+    if not LONG_IMDB_DESCRIPTION:
+        plot = movie.get('plot')
+        if plot and len(plot) > 0:
+            plot = plot[0]
+    else:
+        plot = movie.get('plot outline')
+    if plot and len(plot) > 800:
+        plot = plot[0:800] + "..."
+
+    return {
+        'title': movie.get('title'),
+        'votes': movie.get('votes'),
+        "aka": list_to_str(movie.get("akas")),
+        "seasons": movie.get("number of seasons"),
+        "box_office": movie.get('box office'),
+        'localized_title': movie.get('localized title'),
+        'kind': movie.get("kind"),
+        "imdb_id": f"tt{movie.get('imdbID')}",
+        "cast": list_to_str(movie.get("cast")),
+        "runtime": list_to_str(movie.get("runtimes")),
+        "countries": list_to_str(movie.get("countries")),
+        "certificates": list_to_str(movie.get("certificates")),
+        "languages": list_to_str(movie.get("languages")),
+        "director": list_to_str(movie.get("director")),
+        "writer":list_to_str(movie.get("writer")),
+        "producer":list_to_str(movie.get("producer")),
+        "composer":list_to_str(movie.get("composer")) ,
+        "cinematographer":list_to_str(movie.get("cinematographer")),
+        "music_team": list_to_str(movie.get("music department")),
+        "distributors": list_to_str(movie.get("distributors")),
+        'release_date': date,
+        'year': movie.get('year'),
+        'genres': list_to_str(movie.get("genres")),
+        'poster': movie.get('full-size cover url'),
+        'plot': plot,
+        'rating': str(movie.get("rating")),
+        'url':f'https://www.imdb.com/title/tt{movieid}'
+    }
+    
+
 @Client.on_message(filters.text & filters.private)
 async def movie_handler(client, message):
-    user_text = message.text.strip()
-    if not user_text:
+    query = message.text.strip()
+    if not query:
         return
 
-    wait = await message.reply_text("🔎 Searching IMDb...")
+    wait = await message.reply_text("🔎 IMDb से सही नाम ढूंढ रहा हूँ...")
 
-    correct_name = await asyncio.to_thread(correct_movie_name, user_text)
+    data = await get_poster(query)
 
-    if not correct_name:
+    if not data:
         return await wait.edit(
-            f"❌ IMDb पर कोई result नहीं मिला\n\n"
-            f"Search: `{user_text}`"
+            f"❌ IMDb पर कोई movie नहीं मिली\n\nInput: `{query}`"
         )
 
+    correct_title = data.get("title")
+
     await wait.edit(
-        f"✅ **Correct Movie Name Found**\n\n"
-        f"📝 Your Input: `{user_text}`\n"
-        f"🎬 IMDb Name: **{correct_name}**"
+        f"✅ **Correct Movie Name**\n\n"
+        f"📝 Your Input: `{query}`\n"
+        f"🎬 IMDb Name: **{correct_title}**"
     )
+    
+
